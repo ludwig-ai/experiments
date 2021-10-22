94bc3baec4db316d5de3800bd3a1bf3d2d089f3c
Anne Holler <anne@vmware.com>
2021-10-21 17:58:24 -0700

    Test cases for hyperopt sanity and adding points_to_evaluate

---
+++ automl/heuristics/adult_census_income/hyperopt_tabnet_sanity_ray.sh
@@ -0,0 +1,3 @@
+ludwig hyperopt --config_file config_tabnet_sanity_hyperopt.yaml \
+ --dataset s3://experiments.us-west-2.predibase.com/tabular-experiments/datasets/adult_census_income_1.0/processed/adult_census_income.csv \
+ --output_directory s3://experiments.us-west-2.predibase.com/tabular-experiments/results/
