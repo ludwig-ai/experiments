e485c30c7b0ee09adf8778216ac54be320451322
Anne Holler <anne@vmware.com>
2021-12-01 13:50:05 -0800

    Add artifacts wrt experiments with tabnet, time-budgets, and points to evaluate

---
+++ automl/validation/forest_cover/get_auto_train_config_pte_1hr.py
@@ -0,0 +1,29 @@
+import logging
+import pprint
+
+from ludwig.datasets import forest_cover
+from ludwig.automl import create_auto_config
+
+forest_cover_df = forest_cover.load()
+
+auto_config = create_auto_config(
+    dataset=forest_cover_df,
+    target='Cover_Type',
+    time_limit_s=3600,
+    tune_for_memory=False,
+    user_config={'hyperopt': {'sampler': {'search_alg': {'type': 'random', 'points_to_evaluate': [{
+        'combiner.bn_momentum': 0.6,
+        'combiner.bn_virtual_bs': 4096,
+        'combiner.num_steps': 4,
+        'combiner.output_size': 128,
+        'combiner.relaxation_factor': 1.2,
+        'combiner.size': 32,
+        'combiner.sparsity': 0.000001,
+        'training.batch_size': 4096,
+        'training.decay_rate': 0.9,
+        'training.decay_steps': 20000,
+        'training.learning_rate': 0.01
+        }]}}}}
+)
+
+pprint.pprint(auto_config)
