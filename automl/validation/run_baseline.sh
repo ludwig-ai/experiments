d41ebd81a8f30f07cafc2f015d6e899e1f1d002d
Anne Holler <anne@vmware.com>
2021-12-13 10:45:21 -0800

    Add scripts for running baseline of predibase/elotl experiment

---
+++ automl/validation/run_baseline.sh
@@ -0,0 +1,39 @@
+cd ames_housing
+python3 run_auto_train_1hr_baseline.py >run.1hr 2>&1
+cd ..
+#
+cd forest_cover
+python3 run_auto_train_1hr_baseline.py >run.1hr 2>&1
+cd ..
+#
+cd higgs
+python3 run_auto_train_1hr_baseline.py >run.1hr 2>&1
+cd ..
+#
+#
+cd ames_housing
+python3 run_auto_train_2hr_baseline.py >run.2hr 2>&1
+cd ..
+#
+cd forest_cover
+python3 run_auto_train_2hr_baseline.py >run.2hr 2>&1
+cd ..
+#
+cd higgs
+python3 run_auto_train_2hr_baseline.py >run.2hr 2>&1
+cd ..
+#
+#
+cd ames_housing
+python3 run_auto_train_4hr_baseline.py >run.4hr 2>&1
+cd ..
+#
+cd forest_cover
+python3 run_auto_train_4hr_baseline.py >run.4hr 2>&1
+cd ..
+#
+cd higgs
+python3 run_auto_train_4hr_baseline.py >run.4hr 2>&1
+cd ..
+#
+#
