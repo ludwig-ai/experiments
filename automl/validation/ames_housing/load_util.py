b368f3aa17d73bf423133c8c822b2a29096420da
Anne Holler <anne@vmware.com>
2021-12-30 06:50:56 -0800

    Update after ames_housing test split fix

--- automl/validation/ames_housing/load_util.py
+++ automl/validation/ames_housing/load_util.py
@@ -13,7 +13,13 @@ def load_ames_housing():
         if len(val_df) == 0:
             val_df = train_df.sample(frac=0.1, replace=False, random_state=42)
             train_df = train_df.drop(val_df.index)
-
         val_df.split = 1
+
+        # no test set provided, sample 20% of train set
+        if len(test_df) == 0:
+            test_df = train_df.sample(frac=0.2, replace=False, random_state=42)
+            train_df = train_df.drop(test_df.index)
+        test_df.split = 2
+
         ames_housing_df = pd.concat([train_df, val_df, test_df], ignore_index=True)
         return ames_housing_df
