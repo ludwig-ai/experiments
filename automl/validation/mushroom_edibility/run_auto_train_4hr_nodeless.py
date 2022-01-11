f63d0e03aa5d926f6d9b8ca3edafd8a6a2abc55f
Anne Holler <anne@vmware.com>
2022-01-10 17:12:27 -0800

    Add nodeless scripts for mushroom_edibility

---
+++ automl/validation/mushroom_edibility/run_auto_train_4hr_nodeless.py
@@ -0,0 +1,18 @@
+import logging
+import pprint
+
+from load_util import load_mushroom_edibility
+from ludwig.automl import auto_train
+
+mushroom_edibility_df = load_mushroom_edibility()
+
+auto_train_results = auto_train(
+    dataset=mushroom_edibility_df,
+    target='class',
+    time_limit_s=14400,
+    tune_for_memory=False,
+    output_directory='s3://predibase-runs/nodeless/mushroom_edibility/hours4/',
+    user_config={'hyperopt': {'executor': {'max_concurrent_trials': 3, 'gpu_resources_per_trial': 1, 'cpu_resources_per_trial': 4}}},
+)
+
+pprint.pprint(auto_train_results)
