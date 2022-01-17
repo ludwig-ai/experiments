586983bf51b8aa7cb044ecd7669ae1b111ca39b0
Anne Holler <anne@vmware.com>
2022-01-17 13:16:10 -0800

    Add script for running 1hr auto_train on heuristics datasets

---
+++ automl/heuristics/run1hr.sh
@@ -0,0 +1,48 @@
+cd adult_census_income
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd allstate_claims_severity
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd bnp_claims_management
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd ieee_fraud
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd mercedes_benz_greener
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd otto_group_product
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd poker_hand
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd porto_seguro_safe_driver
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd santander_customer_satisfaction
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd santander_customer_transaction
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd sarcos
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
+cd walmart_recruiting
+python3 run_auto_train_1hr.py >run.1hr 2>&1
+cd ..
+#
