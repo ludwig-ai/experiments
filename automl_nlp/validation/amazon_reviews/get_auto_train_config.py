7b01beeb5cf7b05432f4e2044cfca1e5bea76f35
Anne Holler <anne@vmware.com>
2022-03-26 06:48:59 -0700

    Additional validation dataset

---
+++ automl_nlp/validation/amazon_reviews/get_auto_train_config.py
@@ -0,0 +1,16 @@
+import logging
+import pprint
+
+from load_util import load_amazon_reviews
+from ludwig.automl import create_auto_config
+
+amazon_reviews_df = load_amazon_reviews()
+
+auto_config = create_auto_config(
+    dataset=amazon_reviews_df,
+    target='label',
+    time_limit_s=7200,
+    tune_for_memory=False
+)
+
+pprint.pprint(auto_config)
